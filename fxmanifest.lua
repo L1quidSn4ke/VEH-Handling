92ffaee662ea752de4f80865b435d73028ca1cdc
L1quidSn4ke <blue_lambergini@hotmail.com>
2024-12-19 21:51:45 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Vehicle Handling Based on Tire Type'
+version '1.0.0'
+creator 'WoodlandRP'
+
+client_script 'client.lua'
+server_script 'server.lua'
